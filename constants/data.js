6546d8dfe4fc92339590c1394d61c0fbc1382189
Khush Vasudeo Patil <85348050+AssassinK786@users.noreply.github.com>
2022-01-28 21:31:11 +0530

    Add files via upload

---
+++ constants/data.js
@@ -0,0 +1,119 @@
+const userData = {
+  githubUsername: "Assassink786",
+  name: "Khush Patil",
+  designation: "Cyber Security Enthusiast, IoT & Embedded Developer",
+  avatarUrl: "/avatar.png",
+  email: "khushpatil786@gmail.com",
+  phone: "+91 9930611623",
+  address: "Mumbai, Maharashtra, India.",
+  projects: [
+    {
+      title: "Smart Home with Google Assistant",
+      link: "https://github.com/AssassinK786/herko_WSKhush",
+      desc: "I have created A Google Assistant that excute simple commands using ESP32 Microcontroller & Heroku Application Websockets & Node.js along with Embedded Care used.",
+      imgUrl: "",
+    },
+    {
+      title: "Multi VLAN Design over IPsec VPN for Campus Network",
+      link: "https://github.com/nishchay-0730/Multi-VLAN-Design-over-IPsec-VPN-for-Campus-Network",
+      imgUrl: "/Network Design.png",
+    },
+    {
+      title: "Hotel Sayaji App",
+      link: "https://github.com/AssassinK786/HotelSayajiApp",
+      imgUrl: "",
+    },
+    {
+      title: "V.I.E.H Hybrid Encryptor",
+      link: "https://github.com/AssassinK786/V.I.E.H-Hybrid-Encryptor",
+      imgUrl: "/VIEH Hybrid Encryptor.png",
+    },
+  ],
+  about: {
+    title:
+      "I'm a Cyber Security Enthusiast, IoT & Embedded Developer that loves building products and web applications and secure this applications",
+    description: [
+      `I've been in this field since first year of college. I didn't know what full-stack meant at that time because the term was not coined back then. I'm so out of content right now that I'm literally crying writing this text since lorem text is so 2012 now.`,
+      `After learning HTML and struggling with CSS, I came up with a brilliant idea of using bootstrap so that I don't have to style everything by myself and - for obvious reasons - if you knew bootstrap, you were cool.`,
+      `But now, I'm a legend. I've been coding in React, Angular, Vue, Svelte, Node.js, Express, Fastify, MongoDB, MySQL, Bootstrap, Tailwind, C, C++, C#, Rust, Haskell, Perl, PHP, Assembly, Logo and YouDontKnowShit programming language etc to name a few. Although I barely know the syntax (Psst, Stack overflow!), I consider myself a Ninja developer I'm laughing right now.`,
+    ],
+    currentProject: "Cyber Security",
+    currentProjectUrl: "",
+  },
+  experience: [
+    {
+      title: "Graduation",
+      company: "SIES GST, Navi Mumbai.",
+      year: "2023",
+      companyLink: "https://siesgst.edu.in/",
+      desc: "Major in Electronics and Telecommunication Engineering with a CGPA of 8.723(Sem 4). ",
+    },
+    {
+      title: "Penetration Testing Intern",
+      company: "VIRTUALLY TESTING FOUNDATION.",
+      year: "October - December 2021",
+      companyLink: "https://virtuallytesting.com/",
+      desc: "Learned OWASP Top 10 and its fundamentals & have knowledge of Pentesting Tools. ",
+    },
+    {
+      title: "AWS Security Intern ",
+      company: "VIRTUALLY TESTING FOUNDATION.",
+      year: "June - Sept 2021",
+      companyLink: "https://virtuallytesting.com/",
+      desc: "Got Stong Foundation in AWS Cloud Security.I have Deep understanding of Troubleshooting incidents and managing logs & Securing developments from attacks .",
+    },
+    {
+      title: "Enterprise Tester Intern",
+      company: "VIRTUALLY TESTING FOUNDATION.",
+      year: "Apr - June 2021",
+      companyLink: "https://virtuallytesting.com/",
+      desc: "Learned about Red/Blue Teaming, Threat intelligence Purple Teaming, Advance Threat Persistent, Emulation Plans, MITRE ATT&CK, ATT&CK Navigator.",
+    },
+    {
+      title: "IoT Intern",
+      company: "INTERNITY FOUNDATION",
+      year: "June - Oct 2021",
+      companyLink: "",
+      desc: "Did a Project of Smart Home Assitant using ESP32 & Google Assistant.",
+    },
+    {
+      title: "Cyber Security Intern",
+      company: "MAK SECURITIES",
+      year: "May - July 2021",
+      companyLink: "https://www.maksecurities.co.in/",
+      desc: "Went to Daman to get Hands-on Experience in Industry,Learned How Vulnerability Assessment, Penetration Testing is done along with Documentation.",
+    },
+    {
+      title: "Cyber Security Intern ",
+      company: "VIEH Pvt.Ltd",
+      year: "April - October 2021",
+      companyLink: "https://viehgroup.com/",
+      desc: "Worked on some CTF's, TryHackme, Worked on Programs given in the books. Completed Two Projects as Final Projects for Internship.",
+    },
+    {
+      title: "Cybersecurity Research and Development Intern (IoT Pentesting Domain)",
+      company: "TechHack Technologies",
+      year: "Dec 2020 - Jan 2021",
+      companyLink: "https://techhack.technology/",
+      desc: "Researched About IoT Vulnerabilities and find it's Migitations.",
+    },
+    {
+      title: "High School",
+      company: "PES High School & Jr.College, Thane.",
+      year: "2019",
+      companyLink: "",
+      desc: "Subjects being PCM - 69.23% marks aggregate. Passed Jee Mains, MHTCET, IPMAT IIM Indore Exams.",
+    },
+  ],
+  resumeUrl:
+    "https://drive.google.com/file/d/1xmE3BOmgM7TAOOgVp36xQIQvYDntDYoo/view?usp=sharing",
+  socialLinks: {
+    instagram: "https://instagram.com/assassin786official",
+    twitter: "https://twitter.com/KhushPatil786",
+    linkedin: "https://linkedin.com/in/khush-patil",
+    github: "https://github.com/AssassinK786",
+    facebook: "https://facebook.com/",
+  },
+};
+
+export default userData;
